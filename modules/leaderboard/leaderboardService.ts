98dab656799c932972c813c73fc238ad5ac32b0d
m-mik <hiyo@o2.pl>
2026-06-14 23:58:53 +0200

    Fix findUsers big int error after prisma upgrade

--- modules/leaderboard/leaderboardService.ts
+++ modules/leaderboard/leaderboardService.ts
@@ -23,14 +23,17 @@ export function findUsers(page: number) {
   const limit = leaderboard.usersPerPage
   const offset = limit * page - limit
   return prisma.$queryRaw<UserWithPoints[]>`
-    SELECT u.id, 
-           u.name, 
-           u.image, 
-           COUNT(answer) * ${lesson.pointsPerCorrectAnswer} as points 
-    FROM "Question" q
-    JOIN "Lesson" l ON l.id = q."lessonId"
-    RIGHT OUTER JOIN "User" u ON u.id = l."userId" AND q.answer = 'CORRECT'
-    GROUP BY u.id
+    SELECT 
+      u.id,
+      u.name,
+      u.image,
+      (COALESCE(COUNT(q.answer), 0)::int * ${lesson.pointsPerCorrectAnswer})::int AS points
+    FROM "User" u
+    LEFT JOIN "Lesson" l ON l."userId" = u.id
+    LEFT JOIN "Question" q 
+      ON q."lessonId" = l.id 
+      AND q.answer = 'CORRECT'
+    GROUP BY u.id, u.name, u.image
     ORDER BY points DESC
     LIMIT ${limit}
     OFFSET ${offset};
